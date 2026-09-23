c0224f88d92e43f5f7648e09cf5224cacfb5bdfd
aryasukadanas <140170662+aryasukadanas@users.noreply.github.com>
2026-09-23 15:10:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kuotakilat-v1';
+const CACHE_NAME = 'kuotakilat-v2';
 const assets = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const assets = [
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
@@ -14,10 +15,22 @@ self.addEventListener('install', e => {
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names => Promise.all(
+      names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+    )).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    fetch(e.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+      return response;
+    }).catch(() => caches.match(e.request))
   );
 });
